45a9c4e3f5fad2b1fc793d615ccc411ce7659222
Rafael Zanella <rafaelzanella@gmail.com>
2022-10-05 09:02:48 -0300

    Criacao do formulado de serviços prestados

---
+++ clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../clientes/cliente';
+import { ClientesService } from '../../clientes.service';
+import { Subscriber } from 'rxjs';
+import { ServicoPrestado } from '../servicoPrestado';
+
+@Component({
+  selector: 'app-servico-prestado-form',
+  templateUrl: './servico-prestado-form.component.html',
+  styleUrls: ['./servico-prestado-form.component.css']
+})
+export class ServicoPrestadoFormComponent implements OnInit {
+
+  clientes: Cliente[] = []
+  servico: ServicoPrestado;
+
+  constructor(
+    private clienteService: ClientesService
+  ) { 
+    this.servico = new ServicoPrestado();
+  }
+
+  ngOnInit(): void {
+    this.clienteService
+      .getClientes()
+      .subscribe(response => this.clientes = response);
+  }
+
+  onSubmit(){
+    console.log(this.servico)
+  }
+
+}
